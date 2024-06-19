fb2f2808a664fd2ec4d17d76b1af580c6b3daf37
JoEunSang <sang11032@gmail.com>
2024-06-19 15:24:04 +0900

    [fix] add AWS cross-origin

--- src/main/java/com/example/springserver/config/WebConfig.java
+++ src/main/java/com/example/springserver/config/WebConfig.java
@@ -3,14 +3,15 @@ package com.example.springserver.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET","POST","PUT","DELETE")
-                .allowedHeaders("Authorization","Content-Type")
+                .allowedOrigins("http://localhost:3000", "http://ec2-3-37-125-1.ap-northeast-2.compute.amazonaws.com:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
                 .maxAge(3600);
